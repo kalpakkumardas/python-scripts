db6099780cf2f6eb4dad4fc439f88711453c0990
KALPAK KUMAR DAS <59442275+kalpakkumardas@users.noreply.github.com>
2022-01-21 18:41:28 +0530

    Add files via upload

---
+++ Spirograph/main.py
@@ -0,0 +1,33 @@
+import turtle as t
+import random
+
+tim = t.Turtle()
+t.colormode(255)
+
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    random_colour = (r, g, b)
+    return random_colour
+
+
+tim.speed(0)
+
+
+def draw_spiroraph(size_of_gap):
+    for i in range(int(360 / size_of_gap)):
+        tim.color(random_color())
+        tim.circle(100)
+        tim.setheading(tim.heading() + size_of_gap)
+
+
+n = int(input("How close yoy want the gap between the circles to be : "))
+draw_spiroraph(n)
+screen = t.Screen()
+screen.exitonclick()
+
+
+
+
