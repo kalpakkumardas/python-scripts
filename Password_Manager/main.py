b685783b5bb058bf14c0e6ef5562da70ab9982b9
KALPAK KUMAR DAS <59442275+kalpakkumardas@users.noreply.github.com>
2022-01-31 13:56:09 +0530

    Update main.py

--- Password_Manager/main.py
+++ Password_Manager/main.py
@@ -1,30 +1,27 @@
 from tkinter import *
 from tkinter import messagebox
-import random
+from random import choice, randint, shuffle
 import pyperclip
+import json
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
-def generate_password():
-    # Password Generator Project
 
+# Password Generator Project
+def generate_password():
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
                'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
                'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-    nr_letters = random.randint(8, 10)
-    nr_symbols = random.randint(2, 4)
-    nr_numbers = random.randint(2, 4)
-
-    password_letters = [random.choice(letters) for _ in range(nr_letters)]
-    password_symbols = [random.choice(symbols) for _ in range(nr_symbols)]
-    password_numbers = [random.choice(numbers) for _ in range(nr_numbers)]
+    password_letters = [choice(letters) for _ in range(randint(8, 10))]
+    password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
+    password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
 
     password_list = password_letters + password_symbols + password_numbers
+    shuffle(password_list)
 
-    random.shuffle(password_list)
     password = "".join(password_list)
     password_entry.insert(0, password)
     pyperclip.copy(password)
@@ -32,58 +29,90 @@ def generate_password():
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
-    is_ok = 0
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
+    new_data = {
+        website: {
+            "email": email,
+            "password": password,
+        }
+    }
+
     if len(website) == 0 or len(password) == 0:
-        messagebox.showerror(title="Error", message="Don't leave entries blank")
-
+        messagebox.showinfo(title="Oops", message="Please make sure you haven't left any fields empty.")
     else:
-        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered : \n Email : {email}\n "
-                                                              f"Password : {password}\n Is it ok to save ?")
+        try:
+            with open("data.json", "r") as data_file:
+                # Reading old data
+                data = json.load(data_file)
+        except FileNotFoundError:
+            with open("data.json", "w") as data_file:
+                json.dump(new_data, data_file, indent=4)
+        else:
+            # Updating old data with new data
+            data.update(new_data)
 
-    if is_ok:
-        with open("data.txt", "a") as data_file:
-            data_file.write(f"{website} | {email} | {password}\n")
+            with open("data.json", "w") as data_file:
+                # Saving updated data
+                json.dump(data, data_file, indent=4)
+        finally:
             website_entry.delete(0, END)
             password_entry.delete(0, END)
-            website_entry.focus()
+
+
+# ---------------------------- FIND PASSWORD ------------------------------- #
+def find_password():
+    website = website_entry.get().title()
+    try:
+        with open("data.json") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message="No Data File Found.")
+    else:
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(title=website, message=f"Email: {email}\nPassword: {password}")
+        else:
+            messagebox.showinfo(title="Error", message=f"No details for {website} exists.")
 
 
 # ---------------------------- UI SETUP ------------------------------- #
+
 window = Tk()
 window.title("Password Manager")
-window.config(padx=20, pady=20)
+window.config(padx=50, pady=50)
 
-canvas = Canvas(width=200, height=200)
-photo = PhotoImage(file="logo.png")
-canvas.create_image(100, 100, image=photo)
-canvas.grid(column=1, row=0)
+canvas = Canvas(height=200, width=200)
+logo_img = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=logo_img)
+canvas.grid(row=0, column=1)
 
-website_label = Label(text="Website: ")
-website_label.grid(column=0, row=1)
+# Labels
+website_label = Label(text="Website:")
+website_label.grid(row=1, column=0)
+email_label = Label(text="Email/Username:")
+email_label.grid(row=2, column=0)
+password_label = Label(text="Password:")
+password_label.grid(row=3, column=0)
 
-email_label = Label(text="Email/Username: ")
-email_label.grid(column=0, row=2)
-
-password_label = Label(text="Password: ")
-password_label.grid(column=0, row=3)
-
-website_entry = Entry(width=35)
-website_entry.grid(column=1, row=1, columnspan=2)
-
+# Entries
+website_entry = Entry(width=21)
+website_entry.grid(row=1, column=1)
+website_entry.focus()
 email_entry = Entry(width=35)
-email_entry.grid(column=1, row=2, columnspan=2)
-email_entry.insert(END, " @gmail.com")
-
+email_entry.grid(row=2, column=1, columnspan=2)
+email_entry.insert(0, "  @gmail.com")
 password_entry = Entry(width=21)
-password_entry.grid(column=1, row=3)
+password_entry.grid(row=3, column=1)
 
+# Buttons
+search_button = Button(text="Search", width=13, command=find_password)
+search_button.grid(row=1, column=2)
 generate_password_button = Button(text="Generate Password", command=generate_password)
-generate_password_button.grid(column=2, row=3)
-
+generate_password_button.grid(row=3, column=2)
 add_button = Button(text="Add", width=36, command=save)
-add_button.grid(column=1, row=4, columnspan=2)
+add_button.grid(row=4, column=1, columnspan=2)
 
 window.mainloop()
